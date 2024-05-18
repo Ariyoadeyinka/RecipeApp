458a6d1c3fbda26401602c5de90f3f0019b2a97f
Ariyoadeyinka <ariyoadeyinka73@gmail.com>
2024-05-17 20:10:41 -0700

    New commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from "react";
-import styles from '../css/search.module.css'
+import styles from "../css/Search.module.css";
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "90656c90bda049b4ac85da1c37f92ced";
-export default function Search({setFoodData}) {
+export default function Search({ setFoodData }) {
   const [query, setQuery] = useState("pizza");
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await res.json();
-      console.log(data)
-      setFoodData(data.results)
+      console.log(data);
+      setFoodData(data.results);
     }
     fetchFood();
-  }, [query]); 
+  }, [query]);
   return (
-    <div className={styles.searchContainer}> 
-      <input className={styles.searchBox}
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.searchBox}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         type="text"
